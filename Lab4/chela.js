58cfa95bf9d758deefc3f311ba82aacb90067cc2
juanManuelGonzalezA00572003 <85131308+juanManuelGonzalezA00572003@users.noreply.github.com>
2022-02-17 13:19:30 -0600

    hoka

---
+++ Lab4/chela.js
@@ -0,0 +1,25 @@
+//Variables, constantes y consola
+/*
+var num_chelas=13
+console.log(num_chelas)
+console.info(num_chelas)
+console.warn(num_chelas)
+console.error(num_chelas)*/
+
+for(let i =1; i<=10; i++){
+    console.log(i)
+} //Esto hace que la variable i deje de existir despues de esto, pasa cuando usamos let, si usaramos un var si lo podriamos usar mas adelante en el código, pero no nos conviene ya que el usuario no la puede modificar mas adelante.
+
+//alert("si le picas aceptar te va a cargar la verga")
+
+function beber(){
+    console.log("Vamos a tomar")
+}
+
+beber();
+
+//Funciones anonimas
+
+const disfrutar()=>console .log("disfruta la chela")
+
+
