21804e38bc516591ab1301dd184c69c69cbb5ee5
juanManuelGonzalezA00572003 <85131308+juanManuelGonzalezA00572003@users.noreply.github.com>
2022-03-08 17:41:37 -0600

    Lab 8

--- Lab8/app.js
+++ Lab8/app.js
@@ -1,6 +1,26 @@
-const filesystem= require('fs');
+console.log("LABORATORIO 8");
 
-//filesystem.writeFileSync('hola.txt', 'Hola desde node')
+const filesystem = require('fs');
 
-const arreglo=[500,100,123,1,454,895,741]
-console.log(arreglo.sort)
+const prompt = require('prompt-sync')();
+
+const name = prompt('Escribe lo que deseas visualizar en un archivo de texto');
+  filesystem.writeFileSync('hola.txt', name);
+ 
+
+
+const arreglo = [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000, 50];
+var suma = 0;
+
+console.log("El promedio del siguiente array: [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000, 50]");
+for (let item of arreglo) 
+    suma = suma + item;
+} 
+console.log(suma/arreglo.length);
+
+console.log("Obtener la raíz cuadrada de cada item en el siguiente arreglo: [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000, 50]");
+
+const newArr = arreglo.map(Math.sqrt);
+for (let item of newArr) {
+    console.log(item);
+} 
